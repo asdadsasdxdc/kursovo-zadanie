27e992a42c158742227480a49f0d0ed7614405a7
AlexsanderBG <107072392+AlexsanderBG@users.noreply.github.com>
2022-06-21 17:11:17 +0300

    Create  BusinessTripList.js

---
+++ zadanie2/BusinessTripList.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { FlatList } from "react-native";
+import EventCard from "./EventCard";
+import moment from "moment";
+import { getEvents } from "./EventService";
+
+class EventList extends Component {
+  state = {
+    events: [],
+  };
+
+  componentDidMount() {
+    this.props.navigation.addListener('focus', () => {
+      getEvents().then(events => this.setState({events}));
+    });
+    
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(item => ({
+          ...item,
+          updateTime: Date.now()
+        }))
+      });
+    }, 1000); //1 секунди = 1000 милисекунди
+  }
+
+  render() {
+    return (
+      <View style={styles.listView}>
+        <FlatList
+          data={this.state.events}
+          renderItem={({ item }) => <EventCard eventItem={item} />}
+        ></FlatList>
+        <Button
+          onPress={() => this.props.navigation.navigate("EventForm")}
+          title="Add event"
+        />
+      </View>
+    );
+  }
+}
+
+export default EventList;
+
+const styles = StyleSheet.create({
+  listView: {
+    paddingBottom: 40,
+  },
+});
